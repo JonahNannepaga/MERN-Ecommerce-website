fb01a04f682973aeafde7a98f5733e678e9cac3d
Jonah Nannepaga <jonahnannepaga@Jonahs-MacBook-Pro.local>
2020-10-02 12:22:33 +0530

    🌈 Adding the backend Express Api setup.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import products from '../products';
+import React, { useState, useEffect } from 'react';
+// import products from '../products';
 import Product from '../components/Product';
 import { Col, Row } from 'react-bootstrap';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1 className='text-center'>Latest Products</h1>
