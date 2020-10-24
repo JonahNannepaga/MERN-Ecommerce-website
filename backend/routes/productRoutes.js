fe8f1498dc6e6db8bdf11496b14251011f31d0ad
Jonah Nannepaga <jonahnannepaga@Jonahs-MacBook-Pro.local>
2020-10-24 13:17:40 +0530

    :sparkles: Adding controller folder to clean up the routes.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,10 @@
 const express = require('express');
-const asyncHandler = require('express-async-handler');
 const router = express.Router();
-const Product = require('../models/productModel');
 
-// @desc Fetch all Products
-// @route GET /api/products
-// @access Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    // throw new Error('some error');
-    res.json(products);
-  })
-);
+const {getProducts,getProductbyId} = require('../controllers/productController');
 
-// @desc Fetch single Product
-// @route GET /api/products/:id
-// @access Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductbyId);
 
 module.exports = router;
