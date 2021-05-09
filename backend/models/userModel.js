292923b6e2e952aecd702bbca8b9036a1f575d26
Jonah <jonahsatyaanand@gmail.com>
2021-05-09 10:36:08 +0530

    Creating a JWT and user auth setup.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcryptjs');
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -26,6 +27,9 @@ const userSchema = mongoose.Schema(
   }
 );
 
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword,this.password);
+}
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 connectDB();
@@ -16,13 +17,16 @@ const cors = require('cors');
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
 /* API Server Utilities */
+
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 /* MIDDLEWARE */
 app.use(notFound);
